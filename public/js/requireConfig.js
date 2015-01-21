604e79f0f7bdd2c2ac1487f4a72aa135c34842a5
leedaniel <howbest@gmail.com>
2015-01-21 23:03:55 +0900

    README.md

--- public/js/requireConfig.js
+++ public/js/requireConfig.js
@@ -11,13 +11,32 @@ require.config({
         'angular' : '../../bower_components/angular/angular',
         'angular-route' : '../../bower_components/angular-route/angular-route',
         'angularAMD' : '../../bower_components/angularAMD/angularAMD',
+        'angular-animate': '../../bower_components/angular-animate/angular-animate',
+        'angular-aria': '../../bower_components/angular-aria/angular-aria',
+        'angular-material': '../../bower_components/angular-material/angular-material',
+        'angular-resource': '../../bower_components/angular-resource/angular-resource',
+        'hammer': '../../bower_components/hammerjs/hammer',
         'bootstrap' : '../../bower_components/bootstrap/dist/js/bootstrap'
 
     },
 
     'shim' : {
+        'angular' : {
+            'exports' : 'angular'
+        },
         'angular-route' : ['angular'],
         'angularAMD' : ['angular'],
+        'angular-animate': ['angular'],
+        'angular-aria': ['angular'],
+        'angular-resource': ['angular'],
+        'angular-route': ['angular'],
+        'angular-material': [
+            'angular-animate',
+            'angular-aria',
+            'hammer',
+            'css!../../bower_components/angular-material/angular-material',
+            'css!../../bower_components/angular-material/theme/light-blue-dark-theme'
+        ],
         'bootstrap' : {
             'exports' : 'bootstrap',
             'deps' : ['css!../../bower_components/bootstrap/dist/css/bootstrap',
